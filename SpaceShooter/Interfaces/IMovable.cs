30ceeec1e667790c4d20457051628cde332c0cbc
mahdi <mahbabab@gmai.com>
2026-06-29 16:44:22 +0330

    Interfaces : IMovable : enter some new basic code for this interface include x , y , velocities and one method .

--- SpaceShooter/Interfaces/IMovable.cs
+++ SpaceShooter/Interfaces/IMovable.cs
@@ -6,6 +6,11 @@ namespace SpaceShooter.Interfaces
 {
     public interface IMovable
     {
+        public float X {  get; set; }
+        public float Y { get; set; }
+        public float VelocityX { get; set; }
+        public float VelocityY { get; set; }
 
+        public void UpdateMovement(float deltaTime);
     }
 }
