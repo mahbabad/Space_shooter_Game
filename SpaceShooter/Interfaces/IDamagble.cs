11be6e42087470deaef17d4889c62f80f642617b
mahdi <mahbabab@gmai.com>
2026-06-29 17:06:20 +0330

    Interfaces : IDamagble : enter some new code for damage entity in this file include , health , max health , isdestroyed and one more method for damage

--- SpaceShooter/Interfaces/IDamagble.cs
+++ SpaceShooter/Interfaces/IDamagble.cs
@@ -6,5 +6,10 @@ namespace SpaceShooter.Interfaces
 {
     public interface IDamagble
     {
+        public int Health { get; set; }
+        public int MaxHealth { get; }
+        public bool IsDestroyed { get; }
+
+        public void TakeDamage(int amount);
     }
 }
