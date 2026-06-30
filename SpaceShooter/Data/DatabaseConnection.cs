d032b59257009f527493e86cffe88fe95d4e9cb6
mahdi <mahbabab@gmai.com>
2026-07-01 00:50:41 +0330

    Data : DatabaseConnection : write code for basic config of database and its tables Get connection method and initialDatabase method have been written.

--- SpaceShooter/Data/DatabaseConnection.cs
+++ SpaceShooter/Data/DatabaseConnection.cs
@@ -1,11 +1,87 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Data.Sqlite;
 
 namespace SpaceShooter.Data
 {
     public class DatabaseConnection
     {
+        private readonly string _databasePath;
+        private readonly string _connectionString;
+
+        public DatabaseConnection()
+        {
+            _databasePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data_game.db");
+            _connectionString = $"Data Source={_databasePath}";
+        }
+
+        public SqliteConnection GetConection()
+        {
+            return new SqliteConnection(_connectionString);
+        }
+
+        public void InitialDatabase()
+        {
+            CreateDatabaseFile();
+            CreateTable();
+            initialDatabaseIfNotExsit();
+        }
+
+        private void CreateDatabaseFile()
+        {
+            if (!File.Exists(_databasePath))
+            {
+                using FileStream fs = File.Create(_databasePath);
+            }
+        }
+
+        private void CreateTable()
+        {
+            using var connection = GetConection();
+            connection.Open();
+
+            string createGameStatsTable = @"
+                CREATE TABLE IF NOT EXISTS GameStats (
+                    Id INTEGER PRIMARY KEY,
+                    HighScore INTEGER NOT NULL,
+                    TotalCoins INTEGER NOT NULL
+                );";
+
+
+            string createShopItemTable = @"
+               CREATE TABLE IF NOT EXIST shopItem (
+               Id INTEGER PRIMARY KEY AUTOINCREMENT ,
+               ItemKey TEXT NOT NULL UNIQUE,
+               DisplayName TEXT NOT NULL,
+               Category TEXT NOT NULL,
+               Price INTEGER NOT NULL,
+               IsPurchased INTEGER NOT NULL,
+               IsEquipped INTEGER NOT NULL
+                );";
+
+            using var command = connection.CreateCommand();
+            command.CommandText = createGameStatsTable + createShopItemTable;
+            command.ExecuteNonQuery();
+        }
+
+        private void initialDatabaseIfNotExsit()
+        {
+            using var connection =GetConection();
+            connection.Open();
+
+            string insertDefaultGameStats = @"
+                INSERT INTO GameStats (Id, HighScore, TotalCoins)
+                SELECT 1, 0, 0
+                WHERE NOT EXISTS (
+                    SELECT 1 FROM GameStats WHERE Id = 1
+                );
+            ";
+
+            using var command = connection.CreateCommand();
+            command.CommandText = insertDefaultGameStats;
+            command.ExecuteNonQuery();
+        }
 
     }
 }
