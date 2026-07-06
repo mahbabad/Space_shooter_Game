36a66dc42cde769374a44d053007285a841f9689
mahdi <mahbabab@gmai.com>
2026-07-06 20:32:07 +0330

    Models : BaseEntity : the new code has been written include public abstract class BaseEntity for basic properties and method for any entity in the game.

--- SpaceShooter/Models/BaseEntity.cs
+++ SpaceShooter/Models/BaseEntity.cs
@@ -1,11 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Drawing;
+using SpaceShooter.Interfaces;
 
 namespace SpaceShooter.Models
 {
-    public abstract class BaseEntity
+    public abstract class BaseEntity : IMovable
     {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float VelocityX { get; set; }
+        public float VelocityY { get; set; }
+        public float Width { get; set; }
+        public float Height { get; set; }
+        public bool IsActive { get; set; } = true;
 
+        protected BaseEntity(float x, float y, float width, float height)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+        }
+
+       
+        public virtual void UpdateMovement(float deltaTime)
+        {
+            X += VelocityX * deltaTime;
+            Y += VelocityY * deltaTime;
+        }
+
+
+        public RectangleF GetBounds()
+        {
+            return new RectangleF(X, Y, Width, Height);
+        }
     }
 }
