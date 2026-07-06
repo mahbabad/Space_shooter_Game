381140c129bc1c8f4618e72341785a1c8cc74b5c
mahdi <mahbabab@gmai.com>
2026-07-06 21:31:14 +0330

    Models : Coin.cs : the new code has been written for model coin .

--- SpaceShooter/Models/Coin.cs
+++ SpaceShooter/Models/Coin.cs
@@ -1,11 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SpaceShooter.Interfaces;
 
 namespace SpaceShooter.Models
 {
-    public class Coin
+    public class Coin : BaseEntity, ICollectible
     {
+        public const int Value = 1;
 
+        public bool IsCollected { get; private set; }
+
+        public Coin(float x, float y ):base(x , y, 16f , 16f) 
+        {
+            VelocityX = 0f;
+            VelocityY = 60f;
+        }
+
+        public void Collect()
+        {
+            IsCollected = true;
+            IsActive = false;
+        }
     }
 }
