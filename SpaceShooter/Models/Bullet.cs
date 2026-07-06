60ec6a9f0dc58f132cb57e0980a0e9a1e4af78ed
mahdi <mahbabab@gmai.com>
2026-07-06 21:15:59 +0330

    Models : Bullet : the new code has been written for bullet class include : damage , isplayerbullet , constractor.

--- SpaceShooter/Models/Bullet.cs
+++ SpaceShooter/Models/Bullet.cs
@@ -1,11 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SpaceShooter.Interfaces;
 
 namespace SpaceShooter.Models
 {
-    public class Bullet
+    public class Bullet : BaseEntity
     {
+        public int Damage { get; set; }
 
+        public bool IsPlayerBullet { get; }
+
+        public Bullet(float x, float y, float velocityX, float velocityY,
+                      int damage, bool isPlayerBullet):base(x, y , 4f , 12f)
+        {
+            Damage = damage;
+            IsPlayerBullet = isPlayerBullet;
+        }
     }
 }
