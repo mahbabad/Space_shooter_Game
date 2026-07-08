66aeb106d5f495f4d16ef67e01754e20a2c2415e
mahdi <mahbabab@gmai.com>
2026-07-08 14:36:21 +0330

    Models : some bugs was fixed in CoinClas File. include : coinType and value variables  were added to this file.

--- SpaceShooter/Models/Coin.cs
+++ SpaceShooter/Models/Coin.cs
@@ -1,17 +1,22 @@
-﻿using SpaceShooter.Interfaces;
+﻿using SpaceShooter.Enums;
+
+using SpaceShooter.Interfaces;
 
 namespace SpaceShooter.Models
 {
     public class Coin : BaseEntity, ICollectible
     {
-        public const int Value = 1;
+        public CoinType type;
+
+        public int value => type == CoinType.Gold ? 5 : 1;
 
         public bool IsCollected { get; private set; }
 
-        public Coin(float x, float y ):base(x , y, 16f , 16f) 
+        public Coin(float x, float y ,CoinType type ):base(x , y, 16f , 16f) 
         {
             VelocityX = 0f;
             VelocityY = 60f;
+            this.type = type;
         }
 
         public void Collect()
