a28130ef4e88233e7d1b74fbd38fc1828f514f6e
mahdi <mahbabab@gmai.com>
2026-07-08 15:26:16 +0330

    Models : StandardEnemy : StandardEnemy class was created  and was wtitten for simple Enemy Model.

--- SpaceShooter/Models/StandardEnemy.cs
+++ SpaceShooter/Models/StandardEnemy.cs
@@ -1,11 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace SpaceShooter.Models
+﻿namespace SpaceShooter.Models
 {
-    public class StandardEnemy
+    public class StandardEnemy : BaseEnemy
     {
+        public StandardEnemy(float x, float y)
+            : base(x, y, width: 32, height: 32,
+                   maxHealth: 20,           
+                   scoreValue: 10,         
+                   coinDropChance: 0.30f,
+                   goldCoinChance: 0.10f)
+        {
+            VelocityY = 80f; 
+        }
 
+        public override void UpdateMovement(float deltaTime)
+        {
+            Y += VelocityY * deltaTime;
+        }
     }
 }
