c1049504813b63c8ca28b3f183194dfa24cd8583
mahdi <mahbabab@gmai.com>
2026-07-08 15:01:29 +0330

    Models : BaseEnemy : the new code was written for modeling Base enemy  include : abstract class BaseEnemy

--- SpaceShooter/Models/BaseEnemy.cs
+++ SpaceShooter/Models/BaseEnemy.cs
@@ -1,11 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SpaceShooter.Enums;
+using SpaceShooter.Interfaces;
 
 namespace SpaceShooter.Models
 {
-    public class BaseEnemy
+    public abstract class BaseEnemy : BaseEntity, IDamagble
     {
+        public int Health { get;  set; }
+        public int MaxHealth { get;}
+        public bool IsDestroyed { get { return Health <= 0; } }  
+            
+            
 
+        public int ScoreValue { get; protected set; }
+
+        public float CoinDropChance { get; protected set; }
+
+        public float GoldCoinChance { get; protected set; }
+
+        protected BaseEnemy(float x, float y, float width, float height,
+                            int maxHealth, int scoreValue,
+                            float coinDropChance, float goldCoinChance)
+            : base(x, y, width, height)
+        {
+            MaxHealth = maxHealth;
+            Health = maxHealth;
+            ScoreValue = scoreValue;
+            CoinDropChance = coinDropChance;
+            GoldCoinChance = goldCoinChance;
+        }
+
+        public virtual void TakeDamage(int amount)
+        {
+            if (IsDestroyed) return;
+            Health -= amount;
+            if (Health <= 0)
+            {
+                Health = 0;
+                IsActive = false;
+            }
+        }
+
+        
+        public virtual Coin? TryDropCoin(Random random)
+        {
+            if (random.NextDouble() > CoinDropChance)
+                return null;
+
+            var type = random.NextDouble() < GoldCoinChance ? CoinType.Gold : CoinType.Silver;
+            return new Coin(X + Width / 2f, Y + Height / 2f, type);
+        }
+
+        public abstract override void UpdateMovement(float deltaTime);
     }
 }
