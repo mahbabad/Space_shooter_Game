91599fd06821821c539f2c0f31cb8cbedf08ef0a
mahdi <mahbabab@gmai.com>
2026-07-08 14:30:36 +0330

    Enums : CoinType :  coinType enum was created.

---
+++ SpaceShooter/Enums/CoinType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceShooter.Enums
+{
+    public enum CoinType
+    {
+        Silver,
+        Gold    
+    }
+
+}
