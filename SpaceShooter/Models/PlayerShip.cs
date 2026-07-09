ea0ec9c6aaf51db9623a3151a84fcb57fca89fc9
mahdi <mahbabab@gmai.com>
2026-07-09 23:16:19 +0330

    Models :  rewrite PlayerShipClass for easer code.

--- SpaceShooter/Models/PlayerShip.cs
+++ SpaceShooter/Models/PlayerShip.cs
@@ -1,61 +1,29 @@
 ﻿using System;
 using SpaceShooter.Interfaces;
+using SpaceShooter.Core;
 
 namespace SpaceShooter.Models
 {
     public class PlayerShip : BaseEntity, IDamagble
     {
-        private readonly ShipInfo _info;
-
-        public int Health { get; set; }
+        public int Health { get;  set; }
         public int MaxHealth { get; }
-        public bool IsDestroyed => Health <= 0;
+        public bool IsDestroyed { get { return Health <= 0; } }
 
-        public float Speed => _info.Speed;
-        public string Name => _info.Name;
-        public int ShipId => _info.Id;
-
-        public PlayerShip(ShipInfo info, float startX, float startY,
+        public PlayerShip(float startX, float startY,
                           float width = 50f, float height = 50f)
             : base(startX, startY, width, height)
         {
-            _info = info ?? throw new ArgumentNullException(nameof(info));
-
-            if (info.MaxHealth <= 0)
-                throw new ArgumentException("MaxHealth Must be positive.", nameof(info));
-
-            MaxHealth = info.MaxHealth;
-            Health = info.MaxHealth;
-        }
-
-   
-        public void SetDirection(float dirX, float dirY)
-        {
-            float length = (float)Math.Sqrt(dirX * dirX + dirY * dirY);
-            if (length > 0f)
-            {
-                dirX /= length;
-                dirY /= length;
-            }
-
-            VelocityX = dirX * Speed;
-            VelocityY = dirY * Speed;
-        }
-
-        public override void UpdateMovement(float deltaTime)
-        {
-            X += VelocityX * deltaTime;
-            Y += VelocityY * deltaTime;
+            MaxHealth = Core.GameRules.PlayerMaxHealth;
+            Health = Core.GameRules.PlayerMaxHealth;
         }
 
         public void TakeDamage(int amount)
         {
-            if (amount <= 0 || IsDestroyed)
+            if (amount < 0)
                 return;
 
             Health -= amount;
-            if (Health < 0)
-                Health = 0;
         }
     }
 }
