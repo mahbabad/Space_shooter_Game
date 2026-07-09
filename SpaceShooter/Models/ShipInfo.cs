5e33ca4c725848932cfd77b08ccc4a90df12b023
mahdi <mahbabab@gmai.com>
2026-07-09 15:26:23 +0330

    Models : two new classes for modeling playerShip include : ShipInfo , PlayerShip with code.

---
+++ SpaceShooter/Models/ShipInfo.cs
@@ -0,0 +1,13 @@
+﻿namespace SpaceShooter.Models
+{
+    public class ShipInfo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Speed { get; set; }
+        public int MaxHealth { get; set; }
+        public int Price { get; set; }
+        public bool IsPurchased { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
