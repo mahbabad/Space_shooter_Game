486a987134069a10b89c110072d478402d69dfc2
mahdi <mahbabab@gmai.com>
2026-07-09 22:48:55 +0330

    Core : GameRules.cs :  add some new rules include :  PlayerMoveSpeed ,  PlayerMaxHealth ,  PlayerBulletSpeed , PlayerBulletDamage , PlayerFireRate

--- SpaceShooter/Core/GameRules.cs
+++ SpaceShooter/Core/GameRules.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace SpaceShooter.Core
+﻿namespace SpaceShooter.Core
 {
-    public class GameRules
+    public static class GameRules
     {
+        public const float PlayerMoveSpeed = 300f;
+        public const int PlayerMaxHealth = 9;
 
+        public const float PlayerBulletSpeed = 500f;
+        public const int PlayerBulletDamage = 10;
+        public const float PlayerFireRate = 0.25f;     
     }
 }
