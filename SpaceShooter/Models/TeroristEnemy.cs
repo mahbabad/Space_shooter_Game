5881f63a53628fe27ea8031d08771e855e0e6f25
mahdi <mahbabab@gmai.com>
2026-07-09 11:47:25 +0330

    Models : TeroristEnemy : A new class for modeling teroristEnemy.

--- SpaceShooter/Models/TeroristEnemy.cs
+++ SpaceShooter/Models/TeroristEnemy.cs
@@ -1,11 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace SpaceShooter.Models
 {
-    public class TeroristEnemy
+    public class TeroristEnemy : BaseEnemy
     {
+        private const float ChaseSpeed = 160f;        
+        public const int ExplosionDamage = 35;          
 
-    }
+        private float _targetX;
+        private float _targetY;
+
+        public TeroristEnemy(float x, float y)
+        :base(x, y,
+                width:40f,
+                height:40f,
+                maxHealth: 30,
+                scoreValue : 40,
+                coinDropChance: 0.50f,
+                goldCoinChance:0.30f
+                )
+        {
+            VelocityY = ChaseSpeed;  
+        }
+
+
+        public void SetTarget(float targetX, float targetY)
+        {
+            _targetX = targetX;
+            _targetY = targetY;
+        }
+
+        public override void UpdateMovement(float deltaTime)
+        {
+            float dx = _targetX - X;
+            float dy = _targetY - Y;
+            float distance = (float)Math.Sqrt(dx * dx + dy * dy);
+
+            if (distance > 1f)
+            {
+                X += (dx / distance) * ChaseSpeed * deltaTime;
+                Y += (dy / distance) * ChaseSpeed * deltaTime;
+            }
+            else
+            {
+                Y += ChaseSpeed * deltaTime;
+            }
+        }
+     }
 }
+
+
