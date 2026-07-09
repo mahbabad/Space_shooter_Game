90affebce5f48712b8c6e8f014a427e020667bab
mahdi <mahbabab@gmai.com>
2026-07-09 11:02:30 +0330

    Models : HeavyTankEnemy :  new code was written for modeling HeavyTank.

--- SpaceShooter/Models/HeavyTankEnemy.cs
+++ SpaceShooter/Models/HeavyTankEnemy.cs
@@ -1,11 +1,59 @@
-﻿using System;
+﻿using SpaceShooter.Enums;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SpaceShooter.Models
 {
-    public class HeavyTankEnemy
+    public class HeavyTankEnemy : BaseEnemy
     {
+        private const float ShootInterval = 3f;     
+        private const float BulletSpeed = 150f;     
+        private float _timeSinceLastShot;
+
+        public HeavyTankEnemy(float x, float y)
+            : base(x, y,
+                   width: 64, height: 64,
+                   maxHealth: 200,
+                   scoreValue: 150,
+                   coinDropChance: 0.90f,
+                   goldCoinChance : 0.80f)
+        {
+            VelocityY = 30f;      
+        }
+
+
+        private const int BulletDamage = 25;
+
+        public override List<Bullet> UpdateShooting(float deltaTime)
+        {
+            var bullets = new List<Bullet>();
+            _timeSinceLastShot += deltaTime;
+
+            if (_timeSinceLastShot >= ShootInterval)
+            {
+                _timeSinceLastShot = 0f;
+
+                float centerX = X + Width / 2;
+                float centerY = Y + Height / 2;
+
+                for (int i = 0; i < 8; i++)
+                {
+                    float angle = i * (float)(Math.PI / 4);
+                    bullets.Add(new Bullet(
+                        x: centerX,
+                        y: centerY,
+                        velocityX: BulletSpeed * (float)Math.Cos(angle),
+                        velocityY: BulletSpeed * (float)Math.Sin(angle),
+                        damage: BulletDamage,
+                        isPlayerBullet: false));
+                }
+            }
+            return bullets;
+        }
+
+
+
 
     }
 }
