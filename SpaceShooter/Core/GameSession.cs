7c3e8fde4338ed58732e3776f69f26a0b3b33984
mahdi <mahbabab@gmai.com>
2026-07-10 11:05:17 +0330

    basic code for gameSession class.

--- SpaceShooter/Core/GameSession.cs
+++ SpaceShooter/Core/GameSession.cs
@@ -1,11 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using SpaceShooter.Enums;
+using SpaceShooter.Models;
 
 namespace SpaceShooter.Core
 {
     public class GameSession
     {
+       public int CurrentWave {  get; set; }
+       public int Score { get; set; }
+       public int CoinsCollected { get; set; }
+       public GameStatus Status { get; set; }
 
+        public PlayerShip Player { get; set; }
+        public RectangleF GameArea { get; set; }
+
+
+
+
+        public List<BaseEnemy> ActiveEnemies { get; private set; }
+        public List<Bullet> ActiveBullets { get; private set; }
+        public List<Coin> ActiveCoins { get; private set; }
+
+        public GameSession(PlayerShip P) 
+        {
+            ActiveEnemies = new List<BaseEnemy>();
+            ActiveCoins = new List<Coin>();
+            ActiveBullets = new List<Bullet>();
+            Player = P;
+
+            ResetSession();
+        }
+
+        public void ResetSession()
+        {
+            CurrentWave = 1;
+            Score = 0;
+            CoinsCollected = 0;
+            Status = GameStatus.playing; 
+
+            ActiveEnemies.Clear();
+            ActiveBullets.Clear();
+            ActiveCoins.Clear();
+
+            Player = new PlayerShip(GameArea.Width/2 , GameArea.Height);
+        }
     }
 }
