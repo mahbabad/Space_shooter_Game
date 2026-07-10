41bc714be9d72384468c4bc6904a0ead68557cc6
mahdi <mahbabab@gmai.com>
2026-07-10 11:34:40 +0330

    new code for score managment system was written.

--- SpaceShooter/Core/ScoreManager.cs
+++ SpaceShooter/Core/ScoreManager.cs
@@ -1,11 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace SpaceShooter.Core
 {
     public class ScoreManager
     {
+        
 
+        private readonly GameSession _session;
+
+        public ScoreManager(GameSession session)
+        {
+            ResetScore();
+            _session = session;
+
+        }
+
+        public void AddScore(int points)
+        {
+            if (points > 0)
+            {
+                _session.Score += points;
+            }
+        }
+
+        public void ResetScore()
+        {
+            _session.Score = 0;
+        }
     }
 }
