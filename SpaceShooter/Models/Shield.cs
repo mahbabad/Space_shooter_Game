a803295b14ec88fb143cd47933b727ad72f81c1f
ParsaJaladati <parsa.jaladati.ce@gmail.com>
2026-07-13 16:08:22 +0330

    Add shield class

---
+++ SpaceShooter/Models/Shield.cs
@@ -0,0 +1,23 @@
+﻿using SpaceShooter.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceShooter.Models
+{
+    public class Shield : BaseEntity, ICollectible
+    {
+        public bool IsCollected { get; private set; }
+        public Shield(float x, float y) : base(x, y, 50f, 50f)
+        {
+            VelocityX = 0f;
+            VelocityY = 100f;
+        }
+
+        public void Collect()
+        {
+            IsCollected = true;
+            IsActive = false;
+        }
+    }
+}
